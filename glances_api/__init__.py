836a93947fae0e9a60565d8473a63127ecea8bcb
ludeeus <joasoe@gmail.com>
2018-11-20 23:17:34 +0100

    Adds support for https and basic auth

--- glances_api/__init__.py
+++ glances_api/__init__.py
@@ -12,20 +12,25 @@ import async_timeout
 from . import exceptions
 
 _LOGGER = logging.getLogger(__name__)
-_RESOURCE = 'http://{host}:{port}/api/{version}'
+_RESOURCE = '{schema}://{host}:{port}/api/{version}'
 
 
 class Glances(object):
     """A class for handling the data retrieval."""
 
-    def __init__(self, loop, session, host='localhost', port=61208, version=2):
+    def __init__(self, loop, session, host='localhost', port=61208, version=2,
+                 ssl=False, username=None, password=None):
         """Initialize the connection."""
+        schema = 'https' if ssl else 'http'
         self._loop = loop
         self._session = session
-        self.url = _RESOURCE.format(host=host, port=port, version=version)
+        self.url = _RESOURCE.format(schema=schema, host=host, port=port,
+                                    version=version)
         self.data = None
         self.values = None
         self.plugins = None
+        self.username = username
+        self.password = password
 
     async def get_data(self):
         """Retrieve the data."""
@@ -33,7 +38,11 @@ class Glances(object):
 
         try:
             with async_timeout.timeout(5, loop=self._loop):
-                response = await self._session.get(url)
+                if self.password is None:
+                    response = await self._session.get(url)
+                else:
+                    auth = aiohttp.BasicAuth(self.username, self.password)
+                    response = await self._session.get(url, auth=auth)
 
             _LOGGER.debug("Response from Glances API: %s", response.status)
             self.data = await response.json()
@@ -58,7 +67,11 @@ class Glances(object):
 
         try:
             with async_timeout.timeout(5, loop=self._loop):
-                response = await self._session.get(url)
+                if self.password is None:
+                    response = await self._session.get(url)
+                else:
+                    auth = aiohttp.BasicAuth(self.username, self.password)
+                    response = await self._session.get(url, auth=auth)
 
             _LOGGER.debug("Response from Glances API: %s", response.status)
             self.plugins = await response.json()
