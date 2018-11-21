1a556ad5bd24e426aa8c390d608d1c233aa68575
Fabian Affolter <fabian@affolter-engineering.ch>
2018-11-21 10:05:23 +0100

    Update docstring

--- glances_api/__init__.py
+++ glances_api/__init__.py
@@ -1,8 +1,4 @@
-"""
-Copyright (c) 2017-2018 Fabian Affolter <fabian@affolter-engineering.ch>
-
-Licensed under MIT. All rights reserved.
-"""
+"""Client to interact with the Glances API."""
 import asyncio
 import logging
 
