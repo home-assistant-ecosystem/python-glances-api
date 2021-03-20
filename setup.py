12541bf419fe4d119e832ac732798392dfbeb100
Fabian Affolter <fabian@affolter-engineering.ch>
2021-03-20 20:33:04 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(os.path.join(here, "README.rst"), encoding="utf-8") as readme_file:
 
 setup(
     name="glances_api",
-    version="0.2.0",
+    version="0.2.1",
     description="Python API for interacting with Glances.",
     long_description=long_description,
     url="https://github.com/home-assistant-ecosystem/python-glances-api",
