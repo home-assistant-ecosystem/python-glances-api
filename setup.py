5d9cd81333d583507f8120543ab382f4adfe68f4
Fabian Affolter <fabian@affolter-engineering.ch>
2021-03-20 20:30:22 +0100

    Formatting with black

--- setup.py
+++ setup.py
@@ -10,36 +10,36 @@ except ImportError:
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-with open(os.path.join(here, 'README.rst'), encoding='utf-8') as readme_file:
+with open(os.path.join(here, "README.rst"), encoding="utf-8") as readme_file:
     long_description = readme_file.read()
 
-if sys.argv[-1] == 'publish':
-    os.system('python3 setup.py sdist upload')
+if sys.argv[-1] == "publish":
+    os.system("python3 setup.py sdist upload")
     sys.exit()
 
 setup(
-    name='glances_api',
-    version='0.2.0',
-    description='Python API for interacting with Glances.',
+    name="glances_api",
+    version="0.2.0",
+    description="Python API for interacting with Glances.",
     long_description=long_description,
-    url='https://github.com/home-assistant-ecosystem/python-glances-api',
-    download_url='https://github.com/home-assistant-ecosystem/python-glances-api/releases',
-    author='Fabian Affolter',
-    author_email='fabian@affolter-engineering.ch',
-    license='MIT',
-    install_requires=['aiohttp>=3.7.4,<4', 'async_timeout<4'],
-    packages=['glances_api'],
+    url="https://github.com/home-assistant-ecosystem/python-glances-api",
+    download_url="https://github.com/home-assistant-ecosystem/python-glances-api/releases",
+    author="Fabian Affolter",
+    author_email="fabian@affolter-engineering.ch",
+    license="MIT",
+    install_requires=["aiohttp>=3.7.4,<4", "async_timeout<4"],
+    packages=["glances_api"],
     zip_safe=True,
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: MacOS :: MacOS X',
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: POSIX',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Topic :: Utilities',
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Utilities",
     ],
 )
