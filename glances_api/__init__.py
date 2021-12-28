001cc0ea7dbeeec10c2859e347967dcf0c1576da
Guy Khmelnitsky <3136012+GuyKh@users.noreply.github.com>
2021-12-28 10:15:01 +0200

    Update __init__.py

--- glances_api/__init__.py
+++ glances_api/__init__.py
@@ -40,7 +40,7 @@ class Glances(object):
         """Retrieve the data."""
         url = "{}/{}".format(self.url, endpoint)
 
-        httpx_client = self.httpx_client if self.httpx_client else httpx.AsyncClient(verify=self.verify_ss)
+        httpx_client = self.httpx_client if self.httpx_client else httpx.AsyncClient(verify=self.verify_ssl)
         
         try:
             async with httpx_client as client:
