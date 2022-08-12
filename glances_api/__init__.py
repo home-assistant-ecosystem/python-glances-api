fb75883cf347452665c6d0cb1febab5a22e4d048
Rami Mosleh <engrbm87@gmail.com>
2022-08-12 15:16:54 +0300

    Add strict typing Initialize self.data, Check for bad request

--- glances_api/__init__.py
+++ glances_api/__init__.py
@@ -1,58 +1,52 @@
 """Client to interact with the Glances API."""
-import asyncio
+from __future__ import annotations
+
 import logging
+from typing import Any
 
 import httpx
 
 from . import exceptions
 
 _LOGGER = logging.getLogger(__name__)
-_RESOURCE = "{schema}://{host}:{port}/api/{version}"
 
 
-class Glances(object):
+class Glances:
     """A class for handling the data retrieval."""
 
     def __init__(
         self,
-        host="localhost",
-        port=61208,
-        version=2,
-        ssl=False,
-        verify_ssl=True,
-        username=None,
-        password=None,
-        httpx_client=None,
+        host: str = "localhost",
+        port: int = 61208,
+        version: int = 2,
+        ssl: bool = False,
+        verify_ssl: bool = True,
+        username: str | None = None,
+        password: str | None = None,
+        httpx_client: httpx.AsyncClient | None = None,
     ):
         """Initialize the connection."""
         schema = "https" if ssl else "http"
-        self.url = _RESOURCE.format(
-            schema=schema, host=host, port=port, version=version
-        )
-        self.plugins = None
-        self.values = None
+        self.url = f"{schema}://{host}:{port}/api/{version}"
+        self.data: dict[str, Any] = {}
+        self.plugins: list[str] = []
+        self.values: Any | None = None
         self.username = username
         self.password = password
         self.verify_ssl = verify_ssl
-        self.httpx_client = httpx_client
+        self.httpx_client = httpx_client or httpx.AsyncClient(verify=verify_ssl)
 
-    async def get_data(self, endpoint):
+    async def get_data(self, endpoint: str) -> None:
         """Retrieve the data."""
-        url = "{}/{}".format(self.url, endpoint)
-
-        httpx_client = (
-            self.httpx_client
-            if self.httpx_client
-            else httpx.AsyncClient(verify=self.verify_ssl)
-        )
+        url = f"{self.url}/{endpoint}"
 
         try:
-            async with httpx_client as client:
+            async with self.httpx_client as client:
                 if self.password is None:
-                    response = await client.get(str(url))
+                    response = await client.get(url)
                 else:
                     response = await client.get(
-                        str(url), auth=(self.username, self.password)
+                        url, auth=(self.username, self.password)
                     )
         except (httpx.ConnectError, httpx.TimeoutException):
             raise exceptions.GlancesApiConnectionError(f"Connection to {url} failed")
@@ -61,13 +55,16 @@ class Glances(object):
             raise exceptions.GlancesApiAuthorizationError(
                 "Please check your credentials"
             )
-
+        if response.status_code == httpx.codes.BAD_REQUEST:
+            raise exceptions.GlancesApiNoDataAvailable(
+                f"endpoint: '{endpoint}' is not valid"
+            )
         if response.status_code == httpx.codes.OK:
             try:
                 _LOGGER.debug(response.json())
                 if endpoint == "all":
                     self.data = response.json()
-                if endpoint == "pluginslist":
+                elif endpoint == "pluginslist":
                     self.plugins = response.json()
             except TypeError:
                 _LOGGER.error("Can not load data from Glances")
@@ -75,7 +72,7 @@ class Glances(object):
                     "Unable to get the data from Glances"
                 )
 
-    async def get_metrics(self, element):
+    async def get_metrics(self, element: str) -> None:
         """Get all the metrics for a monitored element."""
         await self.get_data("all")
         await self.get_data("pluginslist")
