3d9d332093d5e554ed6aaaa7d8528b3dec2880f0
wittypluck <l@perlat.net>
2024-05-17 10:19:59 +0200

    Add support for Glances v4 network sensors

--- glances_api/__init__.py
+++ glances_api/__init__.py
@@ -155,10 +155,18 @@ class Glances:
             sensor_data["network"] = {}
             for network in networks:
                 time_since_update = network["time_since_update"]
+                # New name of network sensors in Glances v4
+                rx = network.get("bytes_recv_rate_per_sec")
+                tx = network.get("bytes_sent_rate_per_sec")
+                # Compatibility with Glances v3
+                if rx is None and (rx_bytes := network.get("rx")) is not None:
+                    rx = round(rx_bytes / time_since_update)
+                if tx is None and (tx_bytes := network.get("tx")) is not None:
+                    tx = round(tx_bytes / time_since_update)
                 sensor_data["network"][network["interface_name"]] = {
                     "is_up": network.get("is_up"),
-                    "rx": round(network["rx"] / time_since_update),
-                    "tx": round(network["tx"] / time_since_update),
+                    "rx": rx,
+                    "tx": tx,
                     "speed": round(network["speed"] / 1024**3, 1),
                 }
         data = self.data.get("dockers") or self.data.get("containers")
